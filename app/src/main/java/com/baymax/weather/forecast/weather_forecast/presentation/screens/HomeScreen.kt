3452b5a598dd94f54325bdf92777d298a83b6c89
saquib-rapido <91273284+saquib-rapido@users.noreply.github.com>
2023-01-28 21:46:46 +0530

    Minor improvements and bug fixes

--- app/src/main/java/com/baymax/weather/forecast/weather_forecast/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/baymax/weather/forecast/weather_forecast/presentation/screens/HomeScreen.kt
@@ -1,12 +1,20 @@
 package com.baymax.weather.forecast.weather_forecast.presentation.screens
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun HomeScreen() {
-    Column(modifier = Modifier.fillMaxSize()) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0XFF)),
+        Alignment.BottomCenter
+    ) {
     }
 }
